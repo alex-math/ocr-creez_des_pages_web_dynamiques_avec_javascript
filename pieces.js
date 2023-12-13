cd7611d80712a96ee4049eeecdb350ee09f1ca9a
alex-math <alexandre.mathonneau+github@proton>
2023-12-13 14:51:15 +0100

    Ajout de tous les articles à la section fiches

---
+++ pieces.js
@@ -0,0 +1,33 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+for (let i = 0; i < pieces.length; i++) {
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    
+    const imageElement = document.createElement("img");
+    imageElement.src = pieces[i].image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
+    const description = document.createElement("p");
+    description.innerText = pieces[i].description;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+    const disponibilite = document.createElement("p");
+    disponibilite.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(description);
+    pieceElement.appendChild(disponibilite);
+    }
